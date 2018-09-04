c16e30263326ba0b750463bc0e44f1eb24150cdb
raj <raj@raj-PC>
2018-09-04 13:30:30 +0530

    Comment included

--- BackupFolder/src/com/uti/backup/BackupSource.java
+++ BackupFolder/src/com/uti/backup/BackupSource.java
@@ -14,36 +14,40 @@ public class BackupSource {
 
 	public static void main(String[] args){
 
+		//Source Path
 		String srcPackagePath="E:\\Source Path\\";
+		//Destination Path
 		String destPackagePath="F:\\Destination Path\\";
-		
+
+		//Source & Destination Folder Map
 		Map<String,String> srcDestPackageMap=new LinkedHashMap<String,String>();
 		srcDestPackageMap.put("Source Folder", "Destination Folder");
 		Calendar cal = Calendar.getInstance();
 		Date result = cal.getTime();
 		String currentDate=new SimpleDateFormat("yyyyMMddHHmm").format(result);
-		
+
 		try {
-			
+//			Convert files of Source parth to Zip file
 			ZipUtils appZip = null;
 			Iterator it = srcDestPackageMap.entrySet().iterator();
 			while(it.hasNext()){
-				
+
 				Map.Entry mapEnt= (Map.Entry)it.next();
 				System.out.println(destPackagePath+mapEnt.getValue()+"_"+currentDate+".zip Transfering...\n");
-				appZip = new ZipUtils();
-			        appZip.generateFileList(srcPackagePath+mapEnt.getKey(),new File(srcPackagePath+mapEnt.getKey()));
-			        appZip.zipIt(srcPackagePath+mapEnt.getKey(),destPackagePath+mapEnt.getValue()+"_"+currentDate+".zip");
-			        appZip=null;
 				
+				appZip = new ZipUtils();
+				appZip.generateFileList(srcPackagePath+mapEnt.getKey(),new File(srcPackagePath+mapEnt.getKey()));
+				appZip.zipIt(srcPackagePath+mapEnt.getKey(),destPackagePath+mapEnt.getValue()+"_"+currentDate+".zip");
+				appZip=null;
+
 				System.out.println(destPackagePath+mapEnt.getValue()+"_"+currentDate+".zip Transfered\n");
 			}
-			
-		   
-		    System.out.println("All are Transfered");
+
+
+			System.out.println("All are Transfered");
 		} catch (Exception e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
 	}
-	 
+
 }
